b3471a75709d7193b2a4acf14563e3ca0bc38ab0
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-08 19:37:58 +0000

    add rates

--- src/pages/book.js
+++ src/pages/book.js
@@ -14,18 +14,47 @@ const AboutPage = () => (
       image={<HeroImg />}
       cta={{
         to: `https://v2.hallmaster.co.uk/Diary/ViewWeeklyDiary/7630`,
-        text: `Book now`
+        text: `Book online`
       }}
     />
     <Body>
-      <h1>Book</h1>
+      <h1>How to book</h1>
       <p>
         We offer an online booking service or if you prefer, you can contact us
-        via email or phone.
+        via email or phone using the details below
       </p>
+      <h2>Booking secretary</h2>
+      <p>
+        For enquiries related to room bookings/availability/details please
+        contact our Bookings Secretarys Joy and Tim Mason.
+        <a href="mailto:holthallbooking@gmail.com">holthallbooking@gmail.com</a>
+        07421 838343.
+      </p>
+      <p className="mt-4 mb-0">Church View Farm,</p>
+      <p className="mt-0 mb-0">Melksham Road,</p>
+      <p className="mt-0 mb-0">Holt,</p>
+      <p className="mt-0 mb-0">Wiltshire,</p>
+      <p className="mt-0 mb-0">BA14 6QT</p>
       <h2>Rates</h2>
+      <h3>Main hall</h3>
       <ul>
-        <li></li>
+        <li>General use £10.00 per hour - minimum of two hours</li>
+        <li>
+          Fri/Sat evenings - villagers £100.00 - non-villagers £120.00 (access
+          to hall from 5pm)
+        </li>
+        <li>
+          Weddings & Family Parties - villagers £175.00 - non-villagers £200.00
+          (includes use of the kitchen, the bar is hired separately)
+        </li>
+      </ul>
+      <h3>Bar</h3>
+      <ul>
+        <li>£25.00 single charge if alcohol is to be sold across the bar</li>
+      </ul>
+      <h3>Kitchen</h3>
+      <ul>
+        <li>£5.00 per hour</li>
       </ul>
       <h2>Regular hirers</h2>
       <p>
