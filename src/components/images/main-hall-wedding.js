d8f8087257ebfaac55af1d4010011dcc9a232ad2
Doug McDonald <dougajmcdonald@gmail.com>
2024-07-18 21:05:52 +0100

    path error

--- src/components/images/main-hall-wedding.js
+++ src/components/images/main-hall-wedding.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "hall-wedding.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "hall_wedding.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 1200, webpQuality: 100) {
             ...GatsbyImageSharpFluid
